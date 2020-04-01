2bd1fe09585ce5189e3a9c157f25dfb0c11fb675
Tristan Reischl <treischl@gmail.com>
2020-04-01 15:20:50 -0700

    Finish cmp-databiding assignment

--- packages/cmp-databinding-assignment/src/app/app.component.ts
+++ packages/cmp-databinding-assignment/src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  gameTicks: number[] = [];
+
+  onGameStarted() {
+    this.gameTicks.length = 0;
+  }
+
+  onGameInProgress(tick: number) {
+    this.gameTicks.push(tick);
+  }
 }
