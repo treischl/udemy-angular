2c2119395193eb4a93c4bd6e0c000b49851ce6ef
Tristan Reischl <treischl@gmail.com>
2020-05-02 22:28:32 -0700

    Changed event emitters to observable subjects

--- packages/course-project/src/app/recipes/recipe.service.ts
+++ packages/course-project/src/app/recipes/recipe.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, EventEmitter } from "@angular/core";
+import { Injectable } from "@angular/core";
 
 import { Recipe } from "./recipe.model";
 import { Ingredient } from "../shared/ingredient.model";
@@ -6,8 +6,6 @@ import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
 @Injectable({ providedIn: "root" })
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
-
   private _recipes: Recipe[] = [
     new Recipe(
       "Tasy Schnitzel",
