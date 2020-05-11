6df4ead65a4a1e90724246f4b4b1473b40c77d45
Tristan Reischl <treischl@gmail.com>
2020-05-11 15:12:30 -0700

    Complete template-driven forms assignment

--- packages/forms-td-assignment/src/app/app.component.ts
+++ packages/forms-td-assignment/src/app/app.component.ts
@@ -1,8 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {}
+export class AppComponent {
+  @ViewChild("userForm") userForm: NgForm;
+  subscriptionOptions = ["Basic", "Advanced", "Pro"];
+  defaultSubscription = "Advanced";
+
+  onSubmit() {
+    console.log(this.userForm);
+  }
+}
