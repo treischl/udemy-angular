7c2cf69d1fee347d264908ad3e32052f9f8e3824
Tristan Reischl <treischl@gmail.com>
2020-05-18 20:13:32 -0700

    Finish pipes lecture

--- packages/pipes/src/app/app.module.ts
+++ packages/pipes/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
+import { ShortenPipe } from './shorten.pipe';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ShortenPipe, FilterPipe],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
