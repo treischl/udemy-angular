fdba28ec8b8e6515467197cdafac9ea892fd0c0a
Tristan Reischl <treischl@gmail.com>
2020-05-26 21:17:14 -0700

    Setup authentication form

--- packages/course-project/src/app/auth/auth.component.ts
+++ packages/course-project/src/app/auth/auth.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  selector: "app-auth",
+  templateUrl: "./auth.component.html",
+  styleUrls: ["./auth.component.css"],
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent {
+  isLoginMode = true;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
   }
 
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    form.reset();
+  }
 }
