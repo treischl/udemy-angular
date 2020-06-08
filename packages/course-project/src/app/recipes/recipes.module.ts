9f8a2e229434add3a0e6c83a59401cee0bb888a8
Tristan Reischl <treischl@gmail.com>
2020-06-08 15:32:25 -0700

    Refactor shared code into separate module

--- packages/course-project/src/app/recipes/recipes.module.ts
+++ packages/course-project/src/app/recipes/recipes.module.ts
@@ -1,7 +1,7 @@
-import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 
+import { SharedModule } from "../shared/shared.module";
 import { RecipesComponent } from "./recipes.component";
 import { RecipeListComponent } from "./recipe-list/recipe-list.component";
 import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
@@ -19,6 +19,6 @@ import { RecipesRoutingModule } from "./recipes-routing.module";
     RecipeStartComponent,
     RecipeEditComponent,
   ],
-  imports: [CommonModule, ReactiveFormsModule, RecipesRoutingModule],
+  imports: [ReactiveFormsModule, RecipesRoutingModule, SharedModule],
 })
 export class RecipesModule {}
