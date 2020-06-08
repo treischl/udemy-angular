b6dae274193be09367444f59313f307e5f637876
Tristan Reischl <treischl@gmail.com>
2020-06-08 15:45:45 -0700

    Refactor auth code to separate module
    - Also cleaned up a few redundant declarations in _app.module.ts_.

---
+++ packages/course-project/src/app/auth/auth.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+
+import { SharedModule } from "../shared/shared.module";
+import { AuthRoutingModule } from "./auth-routing.module";
+import { AuthComponent } from "./auth.component";
+
+@NgModule({
+  declarations: [AuthComponent],
+  imports: [AuthRoutingModule, FormsModule, SharedModule],
+})
+export class AuthModule {}
