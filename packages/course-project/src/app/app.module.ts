0605f5b53b731130dc0e0cde80351ebea0d33ccf
Tristan Reischl <treischl@gmail.com>
2020-06-08 15:32:25 -0700

    Refactor shared code into separate module

--- packages/course-project/src/app/app.module.ts
+++ packages/course-project/src/app/app.module.ts
@@ -5,26 +5,15 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
-import { DropdownDirective } from "./shared/dropdown.directive";
 import { AppRoutingModule } from "./app-routing.module";
 import { AuthComponent } from "./auth/auth.component";
-import { LoadingSpinnerComponent } from "./shared/loading-spinner/loading-spinner.component";
 import { AuthInterceptorService } from "./auth/auth-interceptor.service";
-import { AlertComponent } from "./shared/alert/alert.component";
-import { PlaceholderDirective } from "./shared/placeholder.directive";
 import { RecipesModule } from "./recipes/recipes.module";
 import { ShoppingListModule } from "./shopping-list/shopping-list.module";
+import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    DropdownDirective,
-    AuthComponent,
-    LoadingSpinnerComponent,
-    AlertComponent,
-    PlaceholderDirective,
-  ],
+  declarations: [AppComponent, HeaderComponent, AuthComponent],
   imports: [
     BrowserModule,
     FormsModule,
@@ -33,6 +22,7 @@ import { ShoppingListModule } from "./shopping-list/shopping-list.module";
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
+    SharedModule,
   ],
   providers: [
     {
