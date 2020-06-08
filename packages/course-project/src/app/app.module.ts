ea03e959accfbb471df1927b510faaf9c96980b0
Tristan Reischl <treischl@gmail.com>
2020-06-08 15:37:44 -0700

    Move interceptor services to "Core" module

--- packages/course-project/src/app/app.module.ts
+++ packages/course-project/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
@@ -7,10 +7,10 @@ import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { AuthComponent } from "./auth/auth.component";
-import { AuthInterceptorService } from "./auth/auth-interceptor.service";
 import { RecipesModule } from "./recipes/recipes.module";
 import { ShoppingListModule } from "./shopping-list/shopping-list.module";
 import { SharedModule } from "./shared/shared.module";
+import { CoreModule } from "./core.module";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, AuthComponent],
@@ -23,13 +23,7 @@ import { SharedModule } from "./shared/shared.module";
     RecipesModule,
     ShoppingListModule,
     SharedModule,
-  ],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorService,
-      multi: true,
-    },
+    CoreModule,
   ],
   bootstrap: [AppComponent],
 })
