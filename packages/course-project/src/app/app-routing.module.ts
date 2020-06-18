1bed616f9ff8294025578a70e48ab637f638a728
Tristan Reischl <treischl@gmail.com>
2020-06-18 12:43:11 -0700

    Setup SSR with NgUniversal

--- packages/course-project/src/app/app-routing.module.ts
+++ packages/course-project/src/app/app-routing.module.ts
@@ -24,8 +24,9 @@ const appRoutes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(appRoutes, {
-      preloadingStrategy: PreloadAllModules,
-    }),
+    preloadingStrategy: PreloadAllModules,
+    initialNavigation: 'enabled'
+}),
   ],
   exports: [RouterModule],
 })
