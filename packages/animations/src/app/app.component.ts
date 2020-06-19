a788c9121c7c14d99a9152189a1f79ba0ce26fd2
Tristan Reischl <treischl@gmail.com>
2020-06-18 22:16:08 -0700

    Add animated transitions

--- packages/animations/src/app/app.component.ts
+++ packages/animations/src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { trigger, state, style } from "@angular/animations";
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from "@angular/animations";
 import { Component } from "@angular/core";
 
 @Component({
@@ -20,15 +26,88 @@ import { Component } from "@angular/core";
           transform: "translateX(100px)",
         }),
       ),
+      transition("normal <=> highlighted", animate(300)),
+    ]),
+    trigger("wildState", [
+      state(
+        "normal",
+        style({
+          backgroundColor: "red",
+          transform: "translateX(0) scale(1)",
+          borderRadius: "0px",
+        }),
+      ),
+      state(
+        "highlighted",
+        style({
+          backgroundColor: "blue",
+          transform: "translateX(100px) scale(1)",
+          borderRadius: "0px",
+        }),
+      ),
+      state(
+        "shrunken",
+        style({
+          backgroundColor: "green",
+          transform: "translateX(0) scale(0.5)",
+          borderRadius: "0px",
+        }),
+      ),
+      transition("normal => highlighted", animate(300)),
+      transition("highlighted => normal", animate(800)),
+      transition("shrunken <=> *", [
+        style({ backgroundColor: "orange" }),
+        animate(1000, style({ borderRadius: "50px" })),
+        animate(500),
+      ]),
+    ]),
+    trigger("list1", [
+      state(
+        "in",
+        style({
+          opacity: 1,
+          transform: "translateX(0)",
+        }),
+      ),
+      transition("void => *", [
+        style({
+          opacity: 0,
+          transform: "translateX(-100px)",
+        }),
+        animate(300),
+      ]),
+      transition("* => void", [
+        style({
+          opacity: 1,
+          transform: "translateX(0)",
+        }),
+        animate(
+          300,
+          style({
+            opacity: 0,
+            transform: "translateX(-100px)",
+          }),
+        ),
+      ]),
     ]),
   ],
 })
 export class AppComponent {
   state = "normal";
+  wildState = "normal";
   list = ["Milk", "Sugar", "Bread"];
 
   onAnimate() {
-    this.state = this.state == "normal" ? "highlighted" : "normal";
+    this.state = this.toggleState(this.state);
+    this.wildState = this.toggleState(this.wildState);
+  }
+
+  private toggleState(state: string) {
+    return state === "normal" ? "highlighted" : "normal";
+  }
+
+  onShrink() {
+    this.wildState = "shrunken";
   }
 
   onAdd(item) {
