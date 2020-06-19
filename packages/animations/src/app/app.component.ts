37b71afc5057936b39fac87ad5b869646bbeacb0
Tristan Reischl <treischl@gmail.com>
2020-06-18 21:51:02 -0700

    Add simple animation

--- packages/animations/src/app/app.component.ts
+++ packages/animations/src/app/app.component.ts
@@ -1,11 +1,35 @@
+import { trigger, state, style } from "@angular/animations";
 import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
+  animations: [
+    trigger("divState", [
+      state(
+        "normal",
+        style({
+          backgroundColor: "red",
+          transform: "translateX(0)",
+        }),
+      ),
+      state(
+        "highlighted",
+        style({
+          backgroundColor: "blue",
+          transform: "translateX(100px)",
+        }),
+      ),
+    ]),
+  ],
 })
 export class AppComponent {
+  state = "normal";
   list = ["Milk", "Sugar", "Bread"];
+
+  onAnimate() {
+    this.state = this.state == "normal" ? "highlighted" : "normal";
+  }
 
   onAdd(item) {
     this.list.push(item);
